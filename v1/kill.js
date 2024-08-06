d1799f81e067fafba494ce117f49a97ec87924a6
Stupid-Cat-Station <524157223@qq.com>
2024-08-06 14:56:03 +0800

    引用库更改

--- v1/kill.js
+++ v1/kill.js
@@ -14,7 +14,7 @@
 // @match        https://www.logosc.cn/*
 // @match        https://www.focodesign.com/*
 // @match        https://www.logomaker.com.cn/*
-// @require      https://lib.baomitu.com/jquery/1.12.4/jquery.min.js
+// @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // @require      https://greasyfork.org/scripts/448541-dom-to-image-js/code/dom-to-imagejs.js?version=1074759
 // @require      https://update.greasyfork.org/scripts/457525/1134363/html2canvas%20141.js
 // @license      AGPL-3.0
@@ -186,10 +186,81 @@ function createRemarkBtn(){
         tutorialBtnObj.innerHTML = "<i class='catfont benmao-jiaocheng'></i> 看教程";
     killMarkObj.appendChild(tutorialBtnObj);
 }
+//去水印提示（搞定设计）
+function gaodingRemarkTips(){
+    var markTipScreen = document.createElement("div");
+        markTipScreen.setAttribute('class', 'remark-tips-screen');
+    document.body.appendChild(markTipScreen);
+
+    var markTipModal = document.createElement("div");
+        markTipModal.setAttribute('class', 'remark-tips-modal');
+    markTipScreen.appendChild(markTipModal);
+
+    var modalTipTitle = document.createElement("h3");
+        modalTipTitle.setAttribute('class', 'modal-title');
+        modalTipTitle.textContent = "请确认是否添加屏蔽网络请求！";
+    markTipModal.appendChild(modalTipTitle);
+
+    var modalTipInfos = document.createElement("div");
+        modalTipInfos.setAttribute('class', 'modal-infos');
+    markTipModal.appendChild(modalTipInfos);
+
+    var stepOne = document.createElement("div");
+        stepOne.setAttribute('class', 'step-one');
+        stepOne.textContent = "1. 作图完成以后按F12打开开发者工具，打开屏蔽网络请求";
+    modalTipInfos.appendChild(stepOne);
+
+    var stepOneImg = document.createElement("img");
+        stepOneImg.setAttribute('class', 'step-ong-img');
+        stepOneImg.src = "https://www.benmao.vip/public/monkey/images/gdimgs/step_1.png";
+    stepOne.appendChild(stepOneImg);
+
+    var stepTwo = document.createElement("div");
+        stepTwo.setAttribute('class', 'step-two');
+        stepTwo.textContent = "2. 添加屏蔽请求，输入屏蔽地址：blob:https://www.gaoding.com/*_*_*_*_*";
+    modalTipInfos.appendChild(stepTwo);
+
+    var stepTwoImg = document.createElement("img");
+        stepTwoImg.setAttribute('class', 'step-two-img');
+        stepTwoImg.src = "https://www.benmao.vip/public/monkey/images/gdimgs/step_2.png";
+    stepTwo.appendChild(stepTwoImg);
+
+    var stepThree = document.createElement("div");
+        stepThree.setAttribute('class', 'step-three');
+        stepThree.textContent = "3. 勾选请求阻止，刷新页面，此时页面中已经没有水印了";
+    modalTipInfos.appendChild(stepThree);
+
+    var stepThreeImg = document.createElement("img");
+        stepThreeImg.setAttribute('class', 'step-three-img');
+        stepThreeImg.src = "https://www.benmao.vip/public/monkey/images/gdimgs/step_3.png";
+    stepThree.appendChild(stepThreeImg);
+
+    var killtipBtnsObj = document.createElement("div");
+        killtipBtnsObj.setAttribute('class', 'kill-tip-btns');
+    markTipModal.appendChild(killtipBtnsObj);
+
+    var closeBtnObj = document.createElement("span");
+        closeBtnObj.setAttribute('class', 'btn');
+        closeBtnObj.textContent = "关闭";
+        closeBtnObj.addEventListener("click", () => {
+            hideVerifyModal('remark-tips-screen')
+        });
+    killtipBtnsObj.appendChild(closeBtnObj);
+
+    var rekillBtnObj = document.createElement("span");
+        rekillBtnObj.setAttribute('class', 'btn');
+        rekillBtnObj.textContent = "已添加，现在去水印";
+        rekillBtnObj.addEventListener("click", () => {
+            location.reload()
+        });
+    killtipBtnsObj.appendChild(rekillBtnObj);
+}
 //去水印功能
 function killMarks(){
     const doctitle = document.title;
-    if(/(易企秀)/.test(doctitle)){
+    if(/(稿定设计)/.test(doctitle)) {
+        gaodingRemarkTips()
+    }else if (/(易企秀)/.test(doctitle)){
         $("div.eqc-watermark").css("position", "static");
         $(".eqc-wm-close").remove();
         let oldStr = window.document.body.innerHTML;
@@ -214,10 +285,10 @@ function killMarks(){
         $("#rongqi").remove();
         $("#outbuttons").remove();
         $(".control-panel").remove()
-    } else if (/(标小智)/.test(doctitle)) {
+    }else if (/(标小智)/.test(doctitle)) {
         $(".watermarklayer").remove();
         $('#watermark').remove()
-    } else if (/(标智客)/.test(doctitle)) {
+    }else if (/(标智客)/.test(doctitle)) {
         console.log(1111)
         $(".watermark").remove();
     }
